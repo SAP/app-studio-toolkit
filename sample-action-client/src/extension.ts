555f04d382b4799d470e8344387c696cf23f1e90
Tomer Epstein <tomer.epstein@sap.com>
2021-02-02 17:37:48 +0200

    chore: sample action client

---
+++ sample-action-client/src/extension.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode';
+
+export async function activate(context: vscode.ExtensionContext) {
+    const basAPI = await vscode.extensions.getExtension("SAPOSS.bas-platform")?.exports;
+
+    context.subscriptions.push(vscode.commands.registerCommand("perform.action.now", () => {
+        const action = new basAPI.actions.CommandAction();
+        action.name = "workbench.action.openGlobalSettings";
+
+        basAPI.actions.performAction(action);
+    }));
+
+	context.subscriptions.push(vscode.commands.registerCommand("schedule.action", () => {
+        const action = new basAPI.actions.CommandAction();
+        action.name = "workbench.action.openGlobalSettings";
+
+        vscode.window.showInformationMessage(`Action ${action.name} was scheduled to run`,);
+        basAPI.actions.performAction(action, {schedule: true});
+    }));
+}
+
+export function deactivate() {
+}
