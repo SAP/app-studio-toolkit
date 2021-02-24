ea9c79813460f3516de794788fb49cd86dfe7ede
Sergey Kuperman <sergey.kuperman@sap.com>
2021-02-24 13:39:12 +0200

    chore: pr review #2

--- src/actions/client.ts
+++ src/actions/client.ts
@@ -2,23 +2,23 @@ import * as vscode from 'vscode';
 import { IAction } from "./interfaces";
 import { _performAction } from "./performer";
 
-export async function performAction(action: IAction, options?: any): Promise<void> {
+export function performAction(action: IAction, options?: any): Thenable<void> {
     if (options?.schedule) {
-        _scheduleAction(action);
+        return _scheduleAction(action);
     } else {
-        _performAction(action);
+        return _performAction(action);
     }
 }
 
 /** Schedule an action for execution after restart */
-async function _scheduleAction(action: IAction): Promise<void> {
+function _scheduleAction(action: IAction): Thenable<void> {
     const actionsSettings = vscode.workspace.getConfiguration();
     let actionsList: any[] | undefined = actionsSettings.get("actions");
     if (!actionsList) {
         actionsList = [];
     }
     actionsList.push(action);
-    actionsSettings.update("actions", actionsList, vscode.ConfigurationTarget.Workspace).then(() => { }, (reason) => {
+    return actionsSettings.update("actions", actionsList, vscode.ConfigurationTarget.Workspace).then(() => { }, (reason) => {
         console.error(`Couldn't schedule action: ${reason}`);
     });
 }
