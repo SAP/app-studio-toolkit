7c010d20d742349eec9ee09e3ca13166cc5483b9
Tomer Epstein <tomer.epstein@sap.com>
2021-03-14 11:46:23 +0200

    chore: update devDependencies app-studio-toolkit-types 0.0.4

--- sample-action-client/src/extension.ts
+++ sample-action-client/src/extension.ts
@@ -1,17 +1,20 @@
 import * as vscode from 'vscode';
+import { bas, ICommandAction } from '@sap-devx/app-studio-toolkit-types';
 
 export async function activate(context: vscode.ExtensionContext) {
-    const basAPI = await vscode.extensions.getExtension("SAPOSS.app-studio-toolkit")?.exports;
+    const basAPI: typeof bas = await vscode.extensions.getExtension("SAPOSS.app-studio-toolkit")?.exports;
 
     context.subscriptions.push(vscode.commands.registerCommand("perform.action.now", () => {
-        const action = new basAPI.actions.CommandAction();
+        const action: ICommandAction = new basAPI.actions.CommandAction();
+        action.id = "perform.action.now"
         action.name = "workbench.action.openGlobalSettings";
 
         basAPI.actions.performAction(action);
     }));
 
 	context.subscriptions.push(vscode.commands.registerCommand("schedule.action", () => {
-        const action = new basAPI.actions.CommandAction();
+        const action: ICommandAction = new basAPI.actions.CommandAction();
+        action.id = "schedule.action"
         action.name = "workbench.action.openGlobalSettings";
 
         vscode.window.showInformationMessage(`Action ${action.name} was scheduled to run`,);
