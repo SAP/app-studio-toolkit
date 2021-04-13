b3fcd80861333780283d66ea377bce8c6e24c854
i350069 <ira.gordin@sap.com>
2021-04-13 12:05:33 +0300

    feat: add logger

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,19 @@
 import { bas } from './api';
 import { startBasctlServer, closeBasctlServer } from './basctlServer/basctlServer';
 import { ActionsController } from './actions/controller';
+import { initLogger, getLogger } from "./logger/logger";
+import { ExtensionContext } from 'vscode';
 
-export async function activate() {
+export async function activate(context: ExtensionContext) {
+    initLogger(context);
+
     startBasctlServer();
     
     ActionsController.loadActions();
 
     ActionsController.performScheduledActions();
+
+    getLogger().info("The App-Studio-Toolkit Extension is active.");
 
     return bas;
 }
