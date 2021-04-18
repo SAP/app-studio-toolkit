0413484106dcc6858b35f06a1e44726b4e3c936d
Tomer Epstein <57438361+tomer-epstein@users.noreply.github.com>
2021-04-18 13:06:02 +0300

    chore: sample - replace command workbench.action.openGlobalSettings with workbench.action.openSettings

--- sample-action-client/src/extension.ts
+++ sample-action-client/src/extension.ts
@@ -7,7 +7,7 @@ export async function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand("perform.action.now", () => {
         const action: ICommandAction = new basAPI.actions.CommandAction();
         action.id = "perform.action.now"
-        action.name = "workbench.action.openGlobalSettings";
+        action.name = "workbench.action.openSettings";
 
         basAPI.actions.performAction(action);
     }));
@@ -15,7 +15,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand("schedule.action", () => {
         const action: ICommandAction = new basAPI.actions.CommandAction();
         action.id = "schedule.action"
-        action.name = "workbench.action.openGlobalSettings";
+        action.name = "workbench.action.openSettings";
 
         vscode.window.showInformationMessage(`Action ${action.name} was scheduled to run`,);
         basAPI.actions.performAction(action, {schedule: true});
