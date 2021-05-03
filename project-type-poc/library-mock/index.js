56cf27d268785b31f86697c350a731f4886358ac
Shachar Soel <shachar.soel@sap.com>
2021-05-03 21:39:12 +0300

    feat: mock lib imple(wip)

--- project-type-poc/library-mock/index.js
+++ project-type-poc/library-mock/index.js
@@ -1,3 +1,61 @@
+
+class ProjectAPI {
+
+    constructor(opts) {
+        // TODO: How to fill the Project Data?
+        //       How to make this portable?
+        //       should some helper metadata exist on the file system?
+        this.data = undefined
+    }
+
+    /**
+     * @return {Promise<Project | undefined>}
+     */
+    async read() {
+        return this.data
+    }
+}
+
+class Project {
+    /**
+     * @param {object} opts
+     * @param {string} opts.type
+     * @param {string} opts.name
+     * @param {string} opts.path
+     * @param {string} opts.prefix
+     * @param {string} opts.cloudService
+     * @param {string[]} opts.tags
+     * @param {Module[]} opts.modules
+     */
+    constructor(opts) {
+        this.type = opts.type;
+        this.name = opts.name;
+        this.path = opts.path;
+        this.prefix = opts.prefix;
+        this.cloudService = opts.cloudService;
+        this.tags = opts.tags;
+        this.modules = opts.modules;
+    }
+}
+
+class Item {
+    /**
+     * @param {object} opts
+     * @param {string} opts.type
+     * @param {string} opts.name
+     * @param {string} opts.ref
+     * @param {string} opts.path
+     */
+    constructor(opts) {
+        this.type = opts.type;
+        this.name = opts.name;
+        this.ref = opts.ref;
+        this.path = opts.path;
+    }
+}
+
+
+
 module.exports = {
 
     /**
@@ -17,3 +75,5 @@ module.exports = {
 
     }
 }
+
+
