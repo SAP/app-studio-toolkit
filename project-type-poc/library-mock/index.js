3917f7cd52e469322f0ab1cbf8c4c10db220744f
Shachar Soel <shachar.soel@sap.com>
2021-05-03 20:44:36 +0300

    feat: mock lib skeleton

---
+++ project-type-poc/library-mock/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    /**
+     * @returns {Promise<ProjectApi[]>}
+     */
+    getProjects() {
+
+    },
+
+    /**
+     *
+     * @param {Tag} tag
+     *
+     * @returns {Promise<ProjectApi[]>}
+     */
+    getProjectsByTags(tag) {
+
+    }
+}
