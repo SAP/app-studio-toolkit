79e671f3faac4ada19fc0087ec4919876844cabd
Idan Treivish <idan.treivish@sap.com>
2021-07-12 18:47:19 +0300

    chore: add support for islcapenables api|

---
+++ src/apis/validateLCAP.ts
@@ -0,0 +1,20 @@
+import { getLogger } from '../logger/logger';
+
+export async function isLCAPEnabled() : Promise<boolean | undefined> {
+    const logger = getLogger().getChildLogger({label: "isLCAPEnabled"});
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const optionalRequire = require("optional-require")(require);
+    const noSapPlugin = "NO_SAP_PLUGIN_FOUND";
+    const sapPlugin = optionalRequire('@sap/plugin') ?? noSapPlugin;
+  
+    if (sapPlugin === noSapPlugin) {
+        logger.trace("Failed to load @sap/plugin, so returning undefined.");
+        return;
+    } 
+    logger.trace("@sap/plugin successfully loaded.");
+
+    const isLCAPEnabled = await sapPlugin.window.isLCAPEnabled();
+    logger.trace("LCAP enabled successfully received.", {isLCAPEnabled});
+
+    return isLCAPEnabled;
+}
