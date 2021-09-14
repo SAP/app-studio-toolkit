99b2f77464b6089197234fd6d98e69083a54fe78
bd82 <shachar.soel@sap.com>
2021-09-14 13:12:58 +0300

    chore: self cr fixes

--- examples/vscode-using-workspace-api/src/extension.ts
+++ examples/vscode-using-workspace-api/src/extension.ts
@@ -9,10 +9,9 @@ async function activate(context: ExtensionContext): Promise<void> {
 
   const workspaceAPI = basToolkitAPI.workspaceAPI;
   const outputChannel = window.createOutputChannel(context.extension.id);
-  // TODO1: add README.
-  // TODO2: Add sample project with meaningful results
   const projects = await workspaceAPI.getProjects();
   if (projects.length > 0) {
+    // naively only print the **first** project found...
     const rootProjectDs = await projects[0].readItems();
     const rootProjectText = JSON.stringify(rootProjectDs, null, "\t");
     outputChannel.appendLine("Found `@sap/artifact-manager` Project:");
