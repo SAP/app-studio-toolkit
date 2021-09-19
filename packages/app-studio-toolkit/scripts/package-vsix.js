a78f910ae907b918e46513aca97fd710b1c04dd3
bd82 <shachar.soel@sap.com>
2021-09-19 14:09:37 +0300

    build: added node_modules to vsix

--- packages/app-studio-toolkit/scripts/package-vsix.js
+++ packages/app-studio-toolkit/scripts/package-vsix.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const { resolve } = require("path");
 const { readFileSync, writeFileSync } = require("fs");
 const { writeJsonSync } = require("fs-extra");
+const proxyquire = require("proxyquire");
 
 const rootExtDir = resolve(__dirname, "..");
 const pkgJsonPath = resolve(rootExtDir, "package.json");
@@ -17,10 +18,16 @@ expect(pkgJson.main).to.equal("./dist/src/extension");
 pkgJson.main = "./dist/extension";
 writeJsonSync(pkgJsonPath, pkgJson, { spaces: 2, EOF: "\n" });
 
-// TODO: wrap this `proxyquire` to inject custom logic to select
-//  the contents of the node_module directory as well.
-//  can we brute force this and select everything from node_modules easily?
-const { packageCommand } = require("vsce/out/package");
+// **Hot-Patching** VSCE using proxyquire.
+// to include the whole `node_modules` directory due to issue with yarn + vsce.
+// note that the `node_modules` directory will only contain the non hoisted packages
+const nodeModulesDir = resolve(rootExtDir, "node_modules");
+const getDepsStub = {
+  getDependencies: async () => [rootExtDir, nodeModulesDir],
+};
+const { packageCommand } = proxyquire("vsce/out/package", {
+  "./npm": getDepsStub,
+});
 // Time to create the VSIX.
 packageCommand({
   cwd: rootExtDir,
