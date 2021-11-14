5e5853f890d3954ab5b80df8c18b11c8dcfd8a09
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2021-11-14 17:51:47 +0200

    chore: add tests and logic

--- packages/npm-dependencies-validation/src/index.ts
+++ packages/npm-dependencies-validation/src/index.ts
@@ -1,4 +1,7 @@
 import { spawn } from "child_process";
+import { resolve, join } from "path";
+import { access, readFile } from "fs/promises";
+import { constants } from "fs";
 import { ProblematicDependency, ProblemType } from "./api";
 
 export class ProjectValidator {
@@ -6,12 +9,48 @@ export class ProjectValidator {
   private readonly LS_DEPS = ["ls", "--depth=0", "--json"]; // dependencies and extraneous modules
   private readonly LS_DEV_DEPS = [...this.LS_DEPS, "--dev"]; // devDependencies
 
-  private getDepsStatus(path: string, devDeps: boolean): Promise<any> {
+  private async isValidJsonFile(packageJsonPath: string): Promise<boolean> {
+    try {
+      const packageJsonContent = await readFile(packageJsonPath, "utf-8");
+      JSON.parse(packageJsonContent);
+      return true;
+    } catch (error: any) {
+      console.debug(
+        `${packageJsonPath} file content is invalid. ${
+          error.stack || error.message
+        }`
+      );
+      return false;
+    }
+  }
+
+  private async isPathExist(packageJsonPath: string): Promise<boolean> {
+    try {
+      await access(packageJsonPath, constants.R_OK | constants.W_OK);
+      return true;
+    } catch (error: any) {
+      console.debug(
+        `${packageJsonPath} file is not accessible. ${
+          error.stack || error.message
+        }`
+      );
+      return false;
+    }
+  }
+
+  private async getDepsStatus(path: string, devDeps: boolean): Promise<any> {
+    const resolvedPath = resolve(path);
+    const packageJsonPath = join(resolve(path), "package.json");
+
+    if (!(await this.isPathExist(packageJsonPath))) return Promise.resolve({});
+    if (!(await this.isValidJsonFile(packageJsonPath)))
+      return Promise.resolve({});
+
     return new Promise((resolve, reject) => {
       const ls = spawn(
         this.NPM,
         devDeps ? [...this.LS_DEV_DEPS] : [...this.LS_DEPS],
-        { cwd: path }
+        { cwd: resolvedPath }
       );
 
       ls.stdout.on("data", (data) => {
@@ -26,11 +65,10 @@ export class ProjectValidator {
     });
   }
 
-  private getProblemType(dependency: any): ProblemType {
+  private getProblemType(dependency: any): ProblemType | undefined {
     if (dependency.missing) return "missing";
     if (dependency.invalid) return "invalid";
-
-    return "extraneous";
+    if (dependency.extraneous) return "extraneous";
   }
 
   private async getProblemticDeps(
@@ -38,20 +76,23 @@ export class ProjectValidator {
     devDeps = false
   ): Promise<ProblematicDependency[]> {
     const { dependencies } = await this.getDepsStatus(path, devDeps);
+
     const problematicDeps: ProblematicDependency[] = [];
     for (const name in dependencies) {
       const dependency = dependencies[name];
-      const version = dependency.version ?? dependency.required;
       const problemType = this.getProblemType(dependency);
-      const problematicDep: ProblematicDependency = {
-        name,
-        version,
-        problemType,
-      };
-      if (problemType != "extraneous") {
-        problematicDep.isDevDependency = devDeps;
+      if (problemType) {
+        const version = dependency.version ?? dependency.required;
+        const problematicDep: ProblematicDependency = {
+          name,
+          version,
+          problemType,
+        };
+        if (problemType != "extraneous") {
+          problematicDep.isDevDependency = devDeps;
+        }
+        problematicDeps.push(problematicDep);
       }
-      problematicDeps.push(problematicDep);
     }
     return problematicDeps;
   }
@@ -67,41 +108,9 @@ export class ProjectValidator {
   }
 }
 
-const pv = new ProjectValidator();
-void pv
-  .getProblematicDependencies(
-    "C:\\wing\\dep-validator\\test\\projects\\no_deps_installed"
-  )
-  .then((result: ProblematicDependency[]) => {
-    console.log(`no_deps_installed: ${result}`);
-  });
-// pv.getStatus("C:\\wing\\dep-validator\\test\\projects\\some_deps_installed").then((result: any) => {
-//     console.log(`some_deps_installed: ${result}`);
-// });
-// void pv.getStatus("C:\\wing\\dep-validator\\test\\projects\\some_deps_redundant").then((result: any) => {
-//     console.log(`some_deps_redundant: ${result}`);
-// });
-console.time("deps status");
-// void pv.getProblematicDependencies("C:\\wing\\yeoman-ui").then((result: any) => {
-//     console.timeEnd("deps status");
-//     console.log(`yeoman-ui: ${JSON.stringify(result, null, 2)}`);
-// });
-
-// TODO: spawn or exec ??
-
-// TODO: check that package.json exists
-
-// TODO: add one more example
-
-// TODO: make test with broken package.json
-
 // TODO: check in BAS on big project CAP or UI5 or Fiori
 
 // TODO: meeting with Ido about performance problems
 
-// use Shachars branch in app-studio-toolkit
-// make packages private:true
-// create 2 packages in app-studio-toolkit
-// create tests and types for problems logic
-// create vscode extension and show errors only for first level package.json projects
+// create vscode extension in app-studio-toolkit and show errors only for first level package.json projects
 // probably use git repos that we talked about for json line search (see in Teams)
--- packages/npm-dependencies-validation/test/npmValidations.spec.ts
+++ packages/npm-dependencies-validation/test/npmValidations.spec.ts
@@ -1,11 +1,89 @@
 import { ProjectValidator } from "../src/index";
+import { expect, assert } from "chai";
 
 describe("ProjectValidator unit test", () => {
-  it("no_deps_installed", async () => {
-    const pv = new ProjectValidator();
+  const pv = new ProjectValidator();
+
+  it("2 dependencies and 1 devDependency declared but are not installed", async () => {
     const result = await pv.getProblematicDependencies(
-      "./projects/no_deps/installed"
+      "./test/projects/no_deps_installed"
     );
-    debugger;
+    expect(result).to.have.lengthOf(3);
+    // missing json-fixer dependency
+    const jsonFixer = result.find((dep) => dep.name === "json-fixer");
+    assert.isDefined(jsonFixer);
+    expect(jsonFixer?.isDevDependency).to.be.false;
+    expect(jsonFixer?.problemType).to.be.equal("missing");
+    expect(jsonFixer?.version).to.be.equal("1.6.12");
+    // missing lodash dependency
+    const lodash = result.find((dep) => dep.name === "lodash");
+    assert.isDefined(lodash);
+    expect(lodash?.isDevDependency).to.be.false;
+    expect(lodash?.problemType).to.be.equal("missing");
+    expect(lodash?.version).to.be.equal("~4.17.21");
+    // missing typescript devDependency
+    const typescript = result.find((dep) => dep.name === "typescript");
+    assert.isDefined(typescript);
+    expect(typescript?.isDevDependency).to.be.true;
+    expect(typescript?.problemType).to.be.equal("missing");
+    expect(typescript?.version).to.be.equal("^4.4.4");
+  });
+
+  it("1 dependency and 1 devDependency declared but are not installed, 1 dependency is installed", async () => {
+    const result = await pv.getProblematicDependencies(
+      "./test/projects/some_deps_installed"
+    );
+    expect(result).to.have.lengthOf(2);
+    // missing lodash dependency
+    const lodash = result.find((dep) => dep.name === "lodash");
+    assert.isDefined(lodash);
+    expect(lodash?.isDevDependency).to.be.false;
+    expect(lodash?.problemType).to.be.equal("missing");
+    expect(lodash?.version).to.be.equal("~4.17.21");
+    // missing typescript devDependency
+    const typescript = result.find((dep) => dep.name === "typescript");
+    assert.isDefined(typescript);
+    expect(typescript?.isDevDependency).to.be.true;
+    expect(typescript?.problemType).to.be.equal("missing");
+    expect(typescript?.version).to.be.equal("^4.4.4");
+  });
+
+  it("1 dependency and 1 devDependency declared but not installed, 1 dependency is installed but redundant", async () => {
+    const result = await pv.getProblematicDependencies(
+      "./test/projects/some_deps_redundant"
+    );
+    expect(result).to.have.lengthOf(3);
+    // missing lodash dependency
+    const lodash = result.find((dep) => dep.name === "lodash");
+    assert.isDefined(lodash);
+    expect(lodash?.isDevDependency).to.be.false;
+    expect(lodash?.problemType).to.be.equal("missing");
+    expect(lodash?.version).to.be.equal("4.17.21");
+    // missing typescript devDependency
+    const typescript = result.find((dep) => dep.name === "typescript");
+    assert.isDefined(typescript);
+    expect(typescript?.isDevDependency).to.be.true;
+    expect(typescript?.problemType).to.be.equal("missing");
+    expect(typescript?.version).to.be.equal("4.4.4");
+    // redundant json-fixer devDependency
+    const jsonFixer = result.find((dep) => dep.name === "json-fixer");
+    assert.isDefined(jsonFixer);
+    expect(jsonFixer?.isDevDependency).to.be.undefined;
+    expect(jsonFixer?.problemType).to.be.equal("extraneous");
+    expect(jsonFixer?.version).to.be.equal("1.6.12");
+  });
+
+  it("invalid package.json", async () => {
+    const result = await pv.getProblematicDependencies(
+      "./test/projects/invalid_package_json"
+    );
+    expect(result).to.have.lengthOf(0);
+  });
+
+  it("no package.json", async () => {
+    const result = await pv.getProblematicDependencies(
+      "./test/projects/no_package_json"
+    );
+    expect(result).to.have.lengthOf(0);
   });
 });
