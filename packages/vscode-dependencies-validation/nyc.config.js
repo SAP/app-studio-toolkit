55ac665adcc58dc692ce931b41c4942942142817
bd82 <shachar.soel@sap.com>
2021-12-21 14:08:05 +0200

    chore: document tests exclusion

--- packages/vscode-dependencies-validation/nyc.config.js
+++ packages/vscode-dependencies-validation/nyc.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   include: "**/src/**",
+  // No integration or end to end tests currently in this project.
+  // `extension.ts` only contains minimal integration code with VSCode.
+  // and can be "safely" excluded.
   exclude: ["src/extension.ts"],
 };
