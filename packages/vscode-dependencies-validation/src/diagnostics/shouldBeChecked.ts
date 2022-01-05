781acdd61a95b93c73f70de055c259ed9e4e2d30
bd82 <shachar.soel@sap.com>
2022-01-05 17:54:09 +0200

    chore: remove todo

--- packages/vscode-dependencies-validation/src/diagnostics/shouldBeChecked.ts
+++ packages/vscode-dependencies-validation/src/diagnostics/shouldBeChecked.ts
@@ -3,7 +3,6 @@ import { isInsideNodeModules } from "../util";
 
 export function shouldBeChecked(path: string): boolean {
   const isPkgJson = endsWith(path, "package.json");
-  // TODO: is inside node_modules needed in the diagnostics flow?
   const isInNodeModules = isInsideNodeModules(path);
   return isPkgJson && !isInNodeModules;
 }
