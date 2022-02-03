4dbadb5f1e382d5db9012f28ea5c076e78bc4890
bd82 <shahars@protonmail.com>
2022-02-03 22:01:21 +0200

    chore: exclude refreshDiagnostics from coverage

--- packages/vscode-dependencies-validation/src/diagnostics/refreshDiagnostics.ts
+++ packages/vscode-dependencies-validation/src/diagnostics/refreshDiagnostics.ts
@@ -3,16 +3,15 @@ import { readFile } from "fs-extra";
 import { findDependencyIssues } from "@sap-devx/npm-dependencies-validation";
 import { convertToDiagnostics } from "./convertToDiagnostics";
 
-// TODO: evaluate if we make a small unit test for this or not? (probably yes... as it does not seem hard)
-
 /**
- * Analyzes package.json file for problems.
+ * Analyzes package.json file for problems and updates the diagnostics collection
+ *
  * @param uri package.json file path to analyze
- * @param dependencyIssueDiagnostics diagnostic collection
+ * @param dependencyIssueDiagnostics vscode diagnostic collection
  */
 export async function refreshDiagnostics(
   uri: Uri,
-  dependencyIssueDiagnostics: DiagnosticCollection
+  dependencyIssueDiagnostics: Pick<DiagnosticCollection, "set">
 ): Promise<void> {
   const pkgJsonPath = uri.fsPath;
   const issues = await findDependencyIssues(pkgJsonPath);
