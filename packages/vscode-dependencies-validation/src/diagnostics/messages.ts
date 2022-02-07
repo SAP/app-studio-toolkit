31f7e3d4e5d552d25a27d9a145ae8b71c85005e4
Shahar Soel <shachar.soel@sap.com>
2022-02-07 16:12:45 +0200

    chore: fix coverage by ignoring design time code

--- packages/vscode-dependencies-validation/src/diagnostics/messages.ts
+++ packages/vscode-dependencies-validation/src/diagnostics/messages.ts
@@ -14,8 +14,10 @@ export function depIssueToDiagnosticMsg(issue: DepIssue): string {
         `The "${issue.name}" package installed version ` +
         `"${issue.actual}", does not match the declared range "${issue.expected}"`
       );
+    /* istanbul ignore next -- design time code */
     default: {
       // this actually works as a design time check using the `never` type
+      /* istanbul ignore next -- design time code */
       throw new UnreachableCaseError(issue);
     }
   }
