14392d9227abd7786b02f983a9b6df3c9f2d29c6
Shahar Soel <shachar.soel@sap.com>
2022-02-16 21:20:39 +0200

    chore: reduce scope

--- packages/vscode-deps-upgrade-tool/src/extension.ts
+++ packages/vscode-deps-upgrade-tool/src/extension.ts
@@ -12,12 +12,14 @@ export function activate(context: ExtensionContext): void {
   //   0. locate metadata for upgrades
   //   1. collect pkg.json files
   //   2. apply upgrades (delay on start? e.g 15mins? or maybe a random range?)
-  //   3. option : apply upgrades AGAIN every specific interval?
   //   4. notifications (grouped?) (what happens to a notification if it is ignored?)
   //   5. vscode settings
-  //   6. exclude by pkg.json property
-  //   7. manual mode (via problems view per opened file or via notifications?)
 
   // const logger = getLogger().getChildLogger({ label: "extension" });
   // logger.info("The Vscode Dependencies Validation Extension is active.");
 }
+
+// Out Of Scope for MVP
+// 3. option : apply upgrades AGAIN every specific interval?
+// 6. exclude by pkg.json property
+// 7. manual mode (via problems view per opened file or via notifications?) (???)
