d738448ec67e03a6a92060e39d223a00f024b714
Esther Perelman <71729183+EstherPerelman@users.noreply.github.com>
2022-05-16 14:46:01 +0300

    fix: support recognize LCAP mode also for vscode (#192)

--- packages/app-studio-toolkit/src/apis/validateLCAP.ts
+++ packages/app-studio-toolkit/src/apis/validateLCAP.ts
@@ -3,12 +3,17 @@ import { extensions } from "vscode";
 
 export const LACP_EXTENSION_ID = "SAPSE.lcap-cockpit";
 
+// eslint-disable-next-line @typescript-eslint/require-await -- the new implementation does not require await.
 export async function isLCAPEnabled(): Promise<boolean> {
+  return isLCAPEnabledSync();
+}
+
+export function isLCAPEnabledSync(): boolean {
   const logger = getLogger().getChildLogger({ label: "isLCAPEnabled" });
 
   // LCAP mode is determined by the existence of the LCAP extension
   const isLCAPEnabled = !!extensions.getExtension(LACP_EXTENSION_ID);
   logger.trace("LCAP enabled mode", { isLCAPEnabled });
 
-  return Promise.resolve(isLCAPEnabled);
+  return isLCAPEnabled;
 }
