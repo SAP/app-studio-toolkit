0db288b179db2c3e9425d331c84f79cf6d5e55d1
Dennis Deng <dzf.hillwind@gmail.com>
2022-12-13 17:48:44 +0800

    feat: update artifact-management from 1.3.0 to 1.17.2 (#200)

--- packages/app-studio-toolkit/src/project-type/workspace-instance.ts
+++ packages/app-studio-toolkit/src/project-type/workspace-instance.ts
@@ -1,5 +1,5 @@
 import { WorkspaceImpl, WorkspaceApi } from "@sap/artifact-management";
-import { workspace } from "vscode";
+import * as vscode from "vscode";
 
 let workspaceAPI: WorkspaceApi;
 
@@ -7,8 +7,7 @@ let workspaceAPI: WorkspaceApi;
  * "Factory" for the `WorkspaceApi` "original" instance.
  */
 export function initWorkspaceAPI(): WorkspaceApi {
-  // @ts-expect-error - we "patched" the `WorkspaceImpl` constructor while waiting for official patch.
-  workspaceAPI = new WorkspaceImpl(workspace);
+  workspaceAPI = new WorkspaceImpl(vscode);
   workspaceAPI.startWatch();
 
   return workspaceAPI;
