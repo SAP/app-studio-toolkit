dca6ecbeb95eee298ddd8737bb8249f4cf7e6091
Neta Molcho <neta.molcho@sap.com>
2023-05-16 09:51:59 +0300

    feat: adding hasFioriCapabilities API

--- packages/app-studio-toolkit/test/apis/validateCapabilities.spec.ts
+++ packages/app-studio-toolkit/test/apis/validateCapabilities.spec.ts
@@ -9,15 +9,17 @@ const testVscode = {
 
 mockVscode(testVscode, "dist/src/logger/logger.js");
 mockVscode(testVscode, "dist/src/apis/validateLCAP.js");
+mockVscode(testVscode, "dist/src/apis/validateFioriCapabilities.js");
 import { isLCAPEnabled, LACP_EXTENSION_ID } from "../../src/apis/validateLCAP";
+import { hasFioriCapabilities, FIORI_EXTENSION_ID } from "../../src/apis/validateFioriCapabilities";
 
-describe("validate LCAP API", () => {
+describe("validate capabilities API", () => {
   it("should return false", async () => {
     const parameterValue = await isLCAPEnabled();
     expect(parameterValue).to.be.false;
   });
 
-  describe("validate returned value according to LCAP extension existence", () => {
+  describe("validate returned value according to the extension existence", () => {
     let extensionsMock: SinonMock;
     let sandbox: SinonSandbox;
 
@@ -53,6 +55,25 @@ describe("validate LCAP API", () => {
         .withExactArgs(LACP_EXTENSION_ID)
         .returns(extension);
       const parameterValue = await isLCAPEnabled();
+      expect(parameterValue).to.be.true;
+    });
+
+    it("should return false when Fiori extension does not exist", async () => {
+      extensionsMock
+        .expects("getExtension")
+        .withExactArgs(FIORI_EXTENSION_ID)
+        .returns(undefined);
+      const parameterValue = await hasFioriCapabilities();
+      expect(parameterValue).to.be.false;
+    });
+
+    it("should return true when Fiori extension exists", async () => {
+      const extension = { id: FIORI_EXTENSION_ID };
+      extensionsMock
+        .expects("getExtension")
+        .withExactArgs(FIORI_EXTENSION_ID)
+        .returns(extension);
+      const parameterValue = await hasFioriCapabilities();
       expect(parameterValue).to.be.true;
     });
   });
