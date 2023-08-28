0e99b3e1745c4178cee06c80b582040a5f908e20
Rima Sirich <rima.sirich@sap.com>
2023-08-28 21:25:49 +0300

    fix: commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 // override commitlint ignores as suggested by https://github.com/dependabot/dependabot-core/issues/2445#issuecomment-949633412
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  ignores: [(message) => /^bump \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
+  ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
 };
