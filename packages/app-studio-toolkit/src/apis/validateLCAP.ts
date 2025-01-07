1006d18e9201c70eddbdbae098196a26e41c47c7
Rima Sirich <rima.sirich@sap.com>
2025-01-07 20:44:32 +0200

    fix: extension name to identify LCAP dev space (#369)

--- packages/app-studio-toolkit/src/apis/validateLCAP.ts
+++ packages/app-studio-toolkit/src/apis/validateLCAP.ts
@@ -1,7 +1,7 @@
 import { getLogger } from "@sap/artifact-management";
 import { extensions } from "vscode";
 
-export const LACP_EXTENSION_ID = "SAPSE.lcap-treeview-container";
+export const LCAP_EXTENSION_ID = "saposs.lcap-guided-development-kit";
 
 // eslint-disable-next-line @typescript-eslint/require-await -- the new implementation does not require await.
 export async function isLCAPEnabled(): Promise<boolean> {
@@ -12,7 +12,7 @@ export function isLCAPEnabledSync(): boolean {
   const logger = getLogger().getChildLogger({ label: "isLCAPEnabled" });
 
   // LCAP mode is determined by the existence of the LCAP extension
-  const isLCAPEnabled = !!extensions.getExtension(LACP_EXTENSION_ID);
+  const isLCAPEnabled = !!extensions.getExtension(LCAP_EXTENSION_ID);
   logger.trace("LCAP enabled mode", { isLCAPEnabled });
 
   return isLCAPEnabled;
