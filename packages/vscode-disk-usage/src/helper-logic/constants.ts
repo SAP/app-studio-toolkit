e891d3432934e1489927fa1de5afcdcdb214a247
Shahar Soel <shachar.soel@sap.com>
2025-07-13 15:35:16 +0300

    test: constants and has-prev-report-expired

--- packages/vscode-disk-usage/src/helper-logic/constants.ts
+++ packages/vscode-disk-usage/src/helper-logic/constants.ts
@@ -1,9 +1,12 @@
-export { HOME_DIR, DISK_USAGE_TIMESTAMP, WS_ID };
+export { HOME_DIR, DISK_USAGE_TIMESTAMP, getWsID };
 
 // "~" did not work perfectly for all shell commands
 // using `/` is safe as this extension is only meant to run under linux
 const HOME_DIR = "/home/user";
 const DISK_USAGE_TIMESTAMP = "bas-disk-usage-report-timestamp";
 
-const wsIdRaw = process.env.WORKSPACE_ID;
-const WS_ID = wsIdRaw ? wsIdRaw.replace("workspaces-ws-", "") : "unknown";
+function getWsID(): string {
+  const wsIdRaw = process.env.WORKSPACE_ID;
+  const id = wsIdRaw ? wsIdRaw.replace("workspaces-ws-", "") : "unknown";
+  return id;
+}
---
+++ packages/vscode-disk-usage/test/helper-logic/has-previous-report-expired.spec.ts
@@ -0,0 +1,72 @@
+import { expect } from "chai";
+import { hasPreviousReportExpired } from "../../src/helper-logic/has-previous-report-expired";
+import { DISK_USAGE_TIMESTAMP } from "../../src/helper-logic/constants";
+import { MementoMap } from "../memento-mock";
+
+describe("has-previous-report-expired module", () => {
+  context("hasPreviousReportExpired() function", () => {
+    const dayInMs = 24 * 60 * 60 * 1000;
+    it("should return true when the previous report has expired", async () => {
+      const globalState = new MementoMap();
+      const eightDaysAgoMs = Date.now() - 8 * dayInMs;
+      await globalState.update(DISK_USAGE_TIMESTAMP, eightDaysAgoMs);
+
+      const result = await hasPreviousReportExpired({
+        globalState,
+        daysBetweenRuns: 7,
+      });
+
+      expect(result).to.be.true;
+    });
+
+    it("should return false when the previous report has not expired", async () => {
+      const globalState = new MementoMap();
+      const fourDaysAgoMs = Date.now() - 4 * dayInMs;
+      await globalState.update(DISK_USAGE_TIMESTAMP, fourDaysAgoMs);
+
+      const result = await hasPreviousReportExpired({
+        globalState,
+        daysBetweenRuns: 7,
+      });
+
+      expect(result).to.be.false;
+    });
+
+    it("should create a 'made up' previous report time if none exists", async () => {
+      const globalState = new MementoMap();
+
+      expect(globalState.get(DISK_USAGE_TIMESTAMP)).to.not.exist;
+      const result = await hasPreviousReportExpired({
+        globalState,
+        daysBetweenRuns: 7,
+      });
+
+      expect(result).to.be.false;
+      const timestampAfter = globalState.get(DISK_USAGE_TIMESTAMP) as number;
+      expect(timestampAfter).to.exist;
+      const sevenDaysAgoInMs = Date.now() - 7 * dayInMs;
+      expect(timestampAfter).to.be.lessThan(Date.now());
+      // `-1000` because the "made up" timestamp is created in the past inside `hasPreviousReportExpired()`
+      expect(timestampAfter - 1000).to.be.greaterThan(sevenDaysAgoInMs);
+    });
+
+    it("should create a 'made up' previous report time if it is an invalid datatype", async () => {
+      const globalState = new MementoMap();
+      await globalState.update(DISK_USAGE_TIMESTAMP, "not-a-number");
+      const result = await hasPreviousReportExpired({
+        globalState,
+        daysBetweenRuns: 7,
+      });
+
+      expect(result).to.be.false;
+      const timestampAfter = globalState.get(DISK_USAGE_TIMESTAMP) as number;
+      expect(timestampAfter).to.exist;
+      const sevenDaysAgoInMs = Date.now() - 7 * dayInMs;
+      expect(timestampAfter).to.be.lessThan(Date.now());
+      // `-1000` because the "made up" timestamp is created in the past inside `hasPreviousReportExpired()`
+      expect(timestampAfter - 1000).to.be.greaterThan(sevenDaysAgoInMs);
+    });
+  });
+
+  context("internal.randomPreviousReportTime() function", () => {});
+});
