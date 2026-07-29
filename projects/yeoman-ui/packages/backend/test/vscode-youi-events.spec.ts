47510da4a1530c313839da1bacdfb735202d7dd2
Oksana Korotkova <oksana.korotkova@sap.com>
2026-07-29 09:42:04 +0100

    fix(yeoman-ui): add .js extensions to test imports for ESM
    - Add .js extensions to relative imports in vscode-youi-events.spec.ts
    - Required for ESM module resolution (moduleResolution: node16)
    - Fixes CI build errors: TS2835 relative import paths need explicit
      file extensions

--- projects/yeoman-ui/packages/backend/test/vscode-youi-events.spec.ts
+++ projects/yeoman-ui/packages/backend/test/vscode-youi-events.spec.ts
@@ -1,4 +1,4 @@
-import { vscode } from "./mockUtil";
+import { vscode } from "./mockUtil.js";
 import { expect } from "chai";
 import { createSandbox, SinonSandbox, SinonMock } from "sinon";
 import * as _ from "lodash";
@@ -7,12 +7,12 @@ import {
   IPromiseCallbacks,
   IRpc,
 } from "@sap-devx/webview-rpc/out.ext/rpc-common";
-import * as messages from "../src/messages";
+import * as messages from "../src/messages.js";
 import { MessageType, Severity, IBannerProps } from "@sap-devx/yeoman-ui-types";
-import { GeneratorOutput } from "../src/vscode-output";
-import { Constants } from "../src/utils/constants";
-import * as loggerWrapper from "../src/logger/logger-wrapper";
-import { VSCodeYouiEvents } from "../src/vscode-youi-events";
+import { GeneratorOutput } from "../src/vscode-output.js";
+import { Constants } from "../src/utils/constants.js";
+import * as loggerWrapper from "../src/logger/logger-wrapper.js";
+import { VSCodeYouiEvents } from "../src/vscode-youi-events.js";
 import * as fs from "fs";
 
 describe("vscode-youi-events unit test", () => {
