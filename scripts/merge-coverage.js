ea1d1d17723c9bb8bbbd25adbb6035882e4dd6af
Jacob Kreyenbuehl <jacob.kreyenbuehl@sap.com>
2026-08-21 10:15:27 +0200

    chore: include projects/* coverage in merge and ignore logger test logs
    - scripts/merge-coverage.js: also collect projects/*/packages/*/.nyc_output so
      nested-project coverage (vscode-logging, yeoman-ui, vscode-mta-tools) is
      included in the merged report; derive the merged file name from the package
      path to avoid collisions between same-named packages across projects.
    - .gitignore: ignore logger's test/.log-out output directory.
    
    Signed-off-by: Jacob Kreyenbuehl <jacob.kreyenbuehl@sap.com>

--- scripts/merge-coverage.js
+++ scripts/merge-coverage.js
@@ -3,7 +3,7 @@
  * This report will can be found at the root `coverage` dir and will also be uploaded to coveralls.io
  * - based on https://github.com/istanbuljs/istanbuljs/blob/1fe490e51909607137ded25b1688581c9fd926cd/monorepo-merge-reports.js
  */
-const { dirname, basename, join, resolve } = require("path");
+const { dirname, join, resolve } = require("path");
 const { spawnSync } = require("child_process");
 
 const rimraf = require("rimraf");
@@ -14,16 +14,20 @@ process.chdir(resolve(__dirname, ".."));
 rimraf.sync(".nyc_output");
 makeDir.sync(".nyc_output");
 
-// Merge coverage data from each package so we can generate a complete reports
-glob.sync("packages/*/.nyc_output").forEach((nycOutput) => {
+// Merge coverage data from each package so we can generate a complete reports.
+// Includes both root-level packages and packages nested under projects/*.
+const nycOutputs = [
+  ...glob.sync("packages/*/.nyc_output"),
+  ...glob.sync("projects/*/packages/*/.nyc_output"),
+];
+nycOutputs.forEach((nycOutput) => {
   const cwd = dirname(nycOutput);
+  // Use the package's path (not just its basename) for the merged file name so
+  // same-named packages across projects (e.g. */packages/types) do not collide.
+  const mergedName = cwd.replace(/[\\/]/g, "__") + ".json";
   const { status, stderr } = spawnSync(
     resolve("node_modules", ".bin", "nyc"),
-    [
-      "merge",
-      ".nyc_output",
-      join(__dirname, "..", ".nyc_output", basename(cwd) + ".json"),
-    ],
+    ["merge", ".nyc_output", join(__dirname, "..", ".nyc_output", mergedName)],
     {
       encoding: "utf8",
       shell: true,
